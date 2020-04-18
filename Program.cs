d995718b533f5d9e5bfb085ceec6768d44c67e3c
Chris Wallis <chris@inybble.com>
2020-04-18 17:37:09 +0100

    Go

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SolarBattMan
+{ 
+    class Program
+    {
+        const string SolarSensorSid = "793983";
+        const string UsageSensorSid = "833355";
+
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            while (true)
+            { 
+                var sensors = await ProcessRepositories();
+
+                var solarSensor = sensors.Where(s => s.Sid == SolarSensorSid).FirstOrDefault();
+                var usageSensor = sensors.Where(s => s.Sid == UsageSensorSid).FirstOrDefault();
+
+                var solarExcess = solarSensor.Value - usageSensor.Value;
+                var sign = solarExcess < 0 ? "" : "+";
+                var msg = string.Format("{0}{1}W", sign, solarExcess);
+
+                Console.WriteLine(msg);
+                Thread.Sleep(10000);
+            }
+        }
+
+        private static async Task<List<Sensor>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "SolarBattMan");
+
+            var streamTask = client.GetStreamAsync("http://www.energyhive.com/mobile_proxy/getCurrentValuesSummary?token=3TAoBW0SidCO90NH3K8rDKP1FVJmhWQB");
+            var sensors = await JsonSerializer.DeserializeAsync<List<Sensor>>(await streamTask);
+            return sensors;
+        }
+    }
+}
---
+++ Sensor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SolarBattMan
+{
+    public class Sensor
+    {
+        [JsonPropertyName("cid")]
+        public string Cid { get; set; }
+
+        [JsonPropertyName("sid")]
+        public string Sid { get; set; }
+
+        [JsonPropertyName("units")]
+        public string Units { get; set; }
+
+        [JsonPropertyName("age")]
+        public int Age { get; set; }
+
+        [JsonPropertyName("data")]
+        public DataValue[] Data { get; set; }
+
+        public int Value
+        {
+            get
+            {
+                if (Data != null)
+                {
+                    foreach (var v in Data[0].TheData.Values)
+                    {
+                        return int.Parse(v.ToString());
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+
+    public class DataValue
+    {
+        [JsonExtensionData]
+        public IDictionary<string, object> TheData { get; set; }
+    }
+}
