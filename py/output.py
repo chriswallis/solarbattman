81941257137371c0e9f02b47597b7a55af465eb6
Chris Wallis <chris@inybble.com>
2020-04-20 22:24:53 +0100

    Added SolarBattMan

---
+++ py/output.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+from enum import Enum
+
+SOLAR_CHARGE_HEADROOM = 200
+OVER_USAGE_HEADROOM = 100
+
+class ActivityState(Enum):
+     IDLE = 0
+     CHARGING = 1
+     POWERING = 2
+
+
+class Output:
+    
+    def __init__(self, name):
+        self.name = name
+
+    def set_sensor_values(self, solar, usage):
+        doCharge = (solar - usage) > SOLAR_CHARGE_HEADROOM
+        doPower = (usage - solar) > OVER_USAGE_HEADROOM
+        doNothing = not doCharge and not doPower
+
+        if doNothing:
+            self._activity_state = ActivityState.IDLE
+
+        if doCharge:
+            self._activity_state = ActivityState.CHARGING
+
+        if doPower:
+            self._activity_state = ActivityState.POWERING
+
+        print("Solar: {0}W  Usage: {1}W".format(solar, usage))
+
+
+class ConsoleOutput(Output):
+    
+    def __init__(self):
+        super(ConsoleOutput, self).__init__("Console Output")
+        print(self.name)
+
+
+class GpioOutput(Output):
+
+    def __init__(self):
+        super(GpioOutput, self).__init__("GPIO Output")
+        print(self.name)
+        GPIO.setwarnings(False)  # Ignore warning for now
+        GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+        GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+
+    def set_sensor_values(self, solar, usage):
+        super().set_sensor_values(solar, usage)
+        GPIO.output(11, GPIO.HIGH if elf._activity_state = ActivityState.CHARGING else GPIO.LOW) 
+        GPIO.output(13, GPIO.HIGH if elf._activity_state = ActivityState.POWERING else GPIO.LOW) 
+        GPIO.output(15, GPIO.HIGH if elf._activity_state = ActivityState.IDLE else GPIO.LOW) 
--- py/solarbattman.py
+++ py/solarbattman.py
@@ -1,29 +1,29 @@
+import output   
 import json
 from time import sleep 
 from urllib.request import urlopen
 
+MONITOR_URL = "http://www.energyhive.com/mobile_proxy/getCurrentValuesSummary?token=3TAoBW0SidCO90NH3K8rDKP1FVJmhWQB"
 SOLAR_SENSOR_SID = "793983"
 USAGE_SENSOR_SID = "833355"
 SOLAR_CORRECTION = -48
 
+c = output.ConsoleOutput()
+
 while True:
-    with urlopen('http://www.energyhive.com/mobile_proxy/getCurrentValuesSummary?token=3TAoBW0SidCO90NH3K8rDKP1FVJmhWQB') as response:
+    with urlopen(MONITOR_URL) as response:
         sensorJson = response.read()
 
     sensors = json.loads(sensorJson)
 
     for s in sensors:
         dataElement = s["data"][0]
-        for key in dataElement.keys():
+        for key in dataElement:
             if s["sid"] == SOLAR_SENSOR_SID:
                 solarNow = dataElement[key] + SOLAR_CORRECTION
             elif s["sid"] == USAGE_SENSOR_SID:
                 usageNow = dataElement[key]
-
-    solarExcess = solarNow - usageNow
-    print(
-        "Solar: " + str(solarNow) + 
-        "W  Usage: " + str(usageNow) + 
-        "W  Solar Excess: " + str(solarExcess) + "W")
     
+    c.set_sensor_values(solarNow, usageNow)
+
     sleep(10)
