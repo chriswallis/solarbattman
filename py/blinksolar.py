5b07f4bd25e6211a1634693f11866d92b8aa44f6
Chris Wallis <chris@inybble.com>
2020-04-20 19:18:18 +0100

    Working on Pi

--- py/blinksolar.py
+++ py/blinksolar.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import json
 from time import sleep 
-from urllib.request import urlopen
+from urllib2 import urlopen
 
 MONITOR_URL = "http://www.energyhive.com/mobile_proxy/getCurrentValuesSummary?token=3TAoBW0SidCO90NH3K8rDKP1FVJmhWQB"
 SOLAR_SENSOR_SID = "793983"
 USAGE_SENSOR_SID = "833355"
 SOLAR_CORRECTION = -48
+SOLAR_CHARGE_HEADROOM = 200
+OVER_USAGE_HEADROOM = 100
 
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
@@ -14,9 +16,7 @@ GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
 
 while True:
-    with urlopen(MONITOR_URL) as response:
-        sensorJson = response.read()
-
+    sensorJson = urlopen(MONITOR_URL).read()
     sensors = json.loads(sensorJson)
 
     for s in sensors:
@@ -30,19 +30,23 @@ while True:
     solarExcess = solarNow - usageNow
     print("Solar: {0}W  Usage: {1}W  Solar Excess: {2}W".format(solarNow, usageNow, solarExcess))
     
-    if solarNow > usageNow:
-        GPIO.output(11, GPIO.HIGH)  # Turn on green
+    doCharge = (solarNow - usageNow) > SOLAR_CHARGE_HEADROOM
+    doPower = (usageNow - solarNow) > OVER_USAGE_HEADROOM
+    doNothing = not doCharge and  not doPower
+
+    if doCharge:
+        GPIO.output(11, GPIO.HIGH)
         GPIO.output(13, GPIO.LOW)
         GPIO.output(15, GPIO.LOW) 
 
-    if solarNow == usageNow:
+    if doPower:
         GPIO.output(11, GPIO.LOW) 
-        GPIO.output(13, GPIO.HIGH)  # Turn on yellow
-        GPIO.output(15, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)  # Turn on yellow
+        GPIO.output(15, GPIO.HIGH)
 
-        if solarNow < usageNow:
+    if doNothing:
         GPIO.output(11, GPIO.LOW) 
-        GPIO.output(13, GPIO.LOW)
-        GPIO.output(15, GPIO.HIGH)  # Turn on red
+        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(15, GPIO.LOW)  # Turn on red
 
     sleep(10)
