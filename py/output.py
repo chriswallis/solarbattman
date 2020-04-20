4d705e734ebe5623b9e1e9294642d413940eed40
Martin West <martin@mjfwest.co.uk>
2020-04-21 00:13:50 +0200

    Added a hacky mock

--- py/output.py
+++ py/output.py
@@ -1,13 +1,31 @@
-import RPi.GPIO as GPIO
-from enum import Enum
+try:
+    import RPi.GPIO as GPIO
+except ModuleNotFoundError as e:
+    class GPIO_class(object):
+        def __init__(self):
+            self.BOARD = 1
+            self.OUT = 69 #LAMO
+            self.LOW = 0
+            self.HIGH = 1
+            
+        def setwarnings(self, something):
+            print(something)
+        def setmode(self, mode):
+            print(f'setmode {mode}')
+        def setup(self, pin, direction, initial):
+            print(f'setup {pin}, {direction}, {initial}')
+        def output(self, pin, level):
+            print(f'output {pin}, {level}')
+            
+    GPIO = GPIO_class()
+    print(e)
 
 SOLAR_CHARGE_HEADROOM = 200
 OVER_USAGE_HEADROOM = 100
 
-class ActivityState(Enum):
-     IDLE = 0
-     CHARGING = 1
-     POWERING = 2
+
+
+ActivityState = {'IDLE':0, "CHRGING":1, "POWERING":2}
 
 
 class Output:
@@ -21,15 +39,15 @@ class Output:
         doNothing = not doCharge and not doPower
 
         if doNothing:
-            self._activity_state = ActivityState.IDLE
+            self._activity_state = ActivityState["IDLE"]
 
         if doCharge:
-            self._activity_state = ActivityState.CHARGING
+            self._activity_state = ActivityState["CHARGING"]
 
         if doPower:
-            self._activity_state = ActivityState.POWERING
+            self._activity_state = ActivityState["POWERING"]
 
-        print("Solar: {0}W  Usage: {1}W".format(solar, usage))
+        print("Solar: {solar}W  Usage: {fish}W".format(solar=solar, fish=usage))
 
 
 class ConsoleOutput(Output):
@@ -53,6 +71,12 @@ class GpioOutput(Output):
 
     def set_sensor_values(self, solar, usage):
         super().set_sensor_values(solar, usage)
-        GPIO.output(11, GPIO.HIGH if self._activity_state == ActivityState.CHARGING else GPIO.LOW) 
-        GPIO.output(13, GPIO.HIGH if self._activity_state == ActivityState.POWERING else GPIO.LOW) 
-        GPIO.output(16, GPIO.HIGH if self._activity_state == ActivityState.IDLE else GPIO.LOW) 
+        GPIO.output(11, GPIO.HIGH if self._activity_state == ActivityState["CHARGING"] else GPIO.LOW) 
+        GPIO.output(13, GPIO.HIGH if self._activity_state == ActivityState["POWERING"] else GPIO.LOW) 
+        GPIO.output(16, GPIO.HIGH if self._activity_state == ActivityState["IDLE"] else GPIO.LOW) 
+
+
+
+
+
+
