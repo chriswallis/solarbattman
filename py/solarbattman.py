0a2464ce68db9a3fe89fad9a59cb5e23835ba2b5
Chris Wallis <chris@inybble.com>
2020-08-30 16:47:43 +0100

    Support OpenEnergyMonitor (Bye bye efergy)

--- py/solarbattman.py
+++ py/solarbattman.py
@@ -4,10 +4,7 @@ import json
 from time import sleep 
 from urllib.request import urlopen
 
-MONITOR_URL = "http://www.energyhive.com/mobile_proxy/getCurrentValuesSummary?token=3TAoBW0SidCO90NH3K8rDKP1FVJmhWQB"
-SOLAR_SENSOR_SID = "793983"
-USAGE_SENSOR_SID = "833355"
-SOLAR_CORRECTION = -52
+MONITOR_URL = "http://emonpi.local/feed/list.json?apikey=8c91f09904345ca35bcb84bac461980b"
 
 logging.basicConfig(filename='output.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
@@ -18,17 +15,19 @@ try:
     with urlopen(MONITOR_URL) as response:
         sensorJson = response.read()
 
-    sensors = json.loads(sensorJson)
+    print(sensorJson)
+    feeds = json.loads(sensorJson)
 
-    for s in sensors:
-        dataElement = s["data"][0]
-        for key in dataElement:
-            if s["sid"] == SOLAR_SENSOR_SID:
-                solarNow = dataElement[key] + SOLAR_CORRECTION
-            elif s["sid"] == USAGE_SENSOR_SID:
-                usageNow = dataElement[key]
-except:
+    for f in feeds:
+        print(f)
+        if f["name"] == "solar":
+            solarNow = f["value"]
+        elif f["name"] == "use":
+            usageNow = f["value"]
+
+except Exception as e:
     print('Getting energy values failed,')
+    print(str(e))
     solarNow = -1
     usageNow = -1
 
