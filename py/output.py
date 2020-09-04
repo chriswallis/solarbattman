0618b0da0ff567ad81e8a29b795ec5fa691dce7c
Chris Wallis <chris@inybble.com>
2020-09-04 16:47:13 +0100

    Framework for EV charging

--- py/output.py
+++ py/output.py
@@ -25,10 +25,14 @@ except ModuleNotFoundError as e:
     GPIO = GPIO_class()
     print(e)
 
-CHARGER_MAX_POWER = 650
-CHARGER_STAY_ON_HEADROOM = 50
 SCRIPT_RUNNING_PIN = 18
+EV_CONTROL_PIN = 13
 CHARGER_CONTROL_PIN = 11
+
+EV_MAX_POWER = 2000
+CHARGER_MAX_POWER = 650
+USAGE_HEADROOM = 50
+
 ERROR = -1
 
 class Output:
@@ -40,16 +44,20 @@ class Output:
 
         # Find out whether the charge pin is already high (ON)
         self.charging = GPIO.input(CHARGER_CONTROL_PIN) == GPIO.HIGH
-
+        self.evCharging = GPIO.input(EV_CONTROL_PIN) == GPIO.HIGH
+  
         # Calculate current solar excess
         solarExcess = solar - usage
 
         if solar == ERROR or usage == ERROR:
             self.doCharge = self.charging
+            self.doEvCharge = self.evCharging
         elif self.charging:
-            self.doCharge = solarExcess > CHARGER_STAY_ON_HEADROOM
-        else:
-            self.doCharge = solarExcess > (CHARGER_MAX_POWER + CHARGER_STAY_ON_HEADROOM)
+            self.doCharge = solarExcess > USAGE_HEADROOM
+            self.doEvCharge = 0
+        else: 
+            self.doCharge = solarExcess > (CHARGER_MAX_POWER + USAGE_HEADROOM)
+            self.doEvCharge = 0
 
         print(f'Solar: {solar}W  Usage: {usage}W  Charging: {self.charging}  Charge now: {self.doCharge}')
 
@@ -64,13 +72,15 @@ class GpioOutput(Output):
         GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
         GPIO.setup(SCRIPT_RUNNING_PIN, GPIO.OUT, initial=GPIO.HIGH)
         GPIO.setup(CHARGER_CONTROL_PIN, GPIO.OUT)
+        GPIO.setup(EV_CONTROL_PIN, GPIO.OUT)
 
     def set_sensor_values(self, solar, usage):
         super().set_sensor_values(solar, usage)
 
         GPIO.output(CHARGER_CONTROL_PIN, GPIO.HIGH if self.doCharge else GPIO.LOW)
-        
-        logging.debug(',%s,%s,%s,%s', solar, usage, 1 if self.charging else 0, 1 if self.doCharge else 0)
+        GPIO.output(EV_CONTROL_PIN, GPIO.HIGH if self.doEvCharge else GPIO.LOW)
+
+        logging.debug(',%s,%s,%s,%s,%s,%s', solar, usage, 1 if self.charging else 0, 1 if self.doCharge else 0, 1 if self.evCharging else 0, 1 if self.doEvCharge else 0)
 
         sleep(1)
         GPIO.output(SCRIPT_RUNNING_PIN, GPIO.LOW)
